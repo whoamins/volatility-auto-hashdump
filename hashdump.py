f23b325d15802cb56c0399a9bcdc989d684a0a7e
whoamins <40am1ns@protonmail.com>
2021-07-13 14:04:59 +0300

    Some bug fix

--- hashdump.py
+++ hashdump.py
@@ -2,10 +2,11 @@ import os
 import sys
 import re
 
-os.system("mkdir output_volatility")
-os.system(f"volatility_2.6_lin64_standalone -f {sys.argv[1]} imageinfo > output_volatility/infovol.txt")
 
-with open("output_volatility/infovol.txt", "r") as file:
+os.system("touch infovol.txt && touch hivevol.txt")
+os.system(f"volatility_2.6_lin64_standalone -f {sys.argv[1]} imageinfo > infovol.txt")
+
+with open("infovol.txt", "r") as file:
 	profiles_list = []
 
 	for line in file:
@@ -14,10 +15,13 @@ with open("output_volatility/infovol.txt", "r") as file:
 		break
 
 
-os.system(f"volatility_2.6_lin64_standalone -f {sys.argv[1]} hivelist --profile={profiles_list[0]} > output_volatility/hivevol.txt")
+try:
+	os.system(f"volatility_2.6_lin64_standalone -f {sys.argv[1]} hivelist --profile={profiles_list[0]} > hivevol.txt")
+except:
+	print("Try to export PATH to your Volatility")
+	sys.exit()
 
-
-with open("output_volatility/hivevol.txt", "r") as file:
+with open("hivevol.txt", "r") as file:
 	addresses_list = []
 
 	for line in file:
@@ -27,9 +31,12 @@ with open("output_volatility/hivevol.txt", "r") as file:
 		if "\SystemRoot\System32\Config\SAM" in line:
 			addresses_list.append(line.split(' ')[:1])
 
+try:
+	os.system(f"volatility_2.6_lin64_standalone -f {sys.argv[1]} --profile={profiles_list[0]} hashdump -y {' '.join(addresses_list[0])} -s {' '.join(addresses_list[1])} > hashes.txt")
+except:
+	print("Try to export PATH to your Volatility")
+	sys.exit()
 
-os.system(f"volatility_2.6_lin64_standalone -f {sys.argv[1]} --profile={profiles_list[0]} hashdump -y {' '.join(addresses_list[0])} -s {' '.join(addresses_list[1])} > output_volatility/hashes.txt")
+os.system(f"john --format=NT --wordlist=/usr/share/wordlists/rockyou.txt hashes.txt")
 
-os.system(f"john --format=NT --wordlist=/usr/share/wordlists/rockyou.txt output_volatility/hashes.txt")
-
-os.system(f"rm output_volatility/hivevol.txt && rm output_volatility/infovol.txt")
+os.system(f"rm hivevol.txt && rm infovol.txt")
